bcb80d699ae215b287118768639b73e754b45ee9
Javier Uncos <javiersrfc30@gmail.com>
2024-01-17 16:35:43 -0300

    creamos logica para obtener producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,7 +1,16 @@
 import { Producto } from "../models/producto"
 
-export const listarProductos = (req,res)=>{
-    res.send("esto es una prueba de peticion get")
+export const listarProductos =async(req,res)=>{
+   try {
+    const productos =await Producto.find()
+    res.status(200).json(productos)
+    
+   } catch (error) {
+    console.log(error)
+    res.status(404).json({
+      mensaje:"error al listar productos"
+    })
+   }
 }
 
 
@@ -18,5 +27,18 @@ export const crearProducto =async (req ,res)=>{
     res.status(404).json({
       mensaje:"ocurrio un error al intentar crear el producto"
     })
+  }
+}
+
+export const obtenerProducto = async(req,res)=>{
+  try {
+   const productoBuscado =await Producto.findById(req.params.id)
+   res.status(200).json(productoBuscado)
+   
+  } catch (error) {
+   console.log(error)
+   res.status(404).json({
+     mensaje:"error no se encontro el producto buscado"
+   })
   }
 }
