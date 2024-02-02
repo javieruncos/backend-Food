b80a63c6ed494b6051ec37a5f74bafee78849315
Javier Uncos <javiersrfc30@gmail.com>
2024-02-02 01:28:06 -0300

    cambios rama usuario

--- src/database.js
+++ src/database.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
 
-const url = "mongodb+srv://JavierSrfc:Riversrfc1010@cluster0.3kg4e1g.mongodb.net/food"
-// const url = "mongodb://localhost:27017/street-food";
+// const url = "mongodb+srv://JavierSrfc:Riversrfc1010@cluster0.3kg4e1g.mongodb.net/food";
+const url = "mongodb://localhost:27017/street-food";
 mongoose.connect(url);
 
 const conexion = mongoose.connection;
 conexion.once("open",()=>{
     console.log("base de datos conectada")
 })
+
+
